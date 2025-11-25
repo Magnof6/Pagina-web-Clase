0e924628acf170a863d93ae81c9d12e28473155a
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-11-25 14:48:22 +0100

    he cambiado la constante usuario para poder almacenar más usuarios

--- express-generator/routes/login.js
+++ express-generator/routes/login.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 /*usuario de ejemplo*/
-const usuario = {
-    username: 'admin',
-    password: 'password'
-};
+const usuarios =[
+    { username: 'admin',password: 'password'},
+    { username: 'user', password: '1234' }
+];
 /* GET login page. */
 router.get('/', (req, res)=> {
     res.render('login', { title: 'Login' , error: null, user: req.session.user });
 });
 
+
 /* POST login., procesar el login */
 router.post('/', (req, res) => {
     const { username, password } = req.body;
-
-    if (username === usuario.username && password === usuario.password) {
+    const userValido = usuarios.find(u => u.username === username && u.password === password);
+    if (userValido) {
         req.session.user = username; // Guardar el usuario en la sesión
         res.redirect('/restringida');
     } else {
